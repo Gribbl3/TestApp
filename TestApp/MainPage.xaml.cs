504f9a7148789800ff122e02df757b87818372b8
Gribbl3 <100309068+Gribbl3@users.noreply.github.com>
2023-08-22 16:45:31 +0800

    Add project files.

---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace TestApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = usernameEntry.Text;
+        }
+
+        private void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            string enteredUsername = usernameEntry.Text;
+            string enteredPassword = passwordEntry.Text;
+
+            //check if both or one of the fields is empty
+            if(String.IsNullOrEmpty(enteredUsername) || String.IsNullOrEmpty(enteredPassword))
+            {
+                errMessage.Text = "Username and/or Password should not be empty. Please try again";
+            }
+            
+             
+            //check if field entered is incorrect 
+            bool isAuthenticated = AuthenticateUser(enteredUsername, enteredPassword);
+
+            if(!isAuthenticated)
+            {   
+                errMessage.Text = "Username and/or Password is incorrect. Please try again.";
+                    
+            }
+            else
+            {
+                errMessage.Text = "Login Successful";
+                errMessage.TextColor = Colors.Green;
+            }
+            
+
+            errMessage.IsVisible = true;
+
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            return username == "admin" && password == "admin";
+        }
+    }
+}
