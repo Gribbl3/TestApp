ca130980a1e7bb0ae342897336f83f290a905546
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-02 21:51:19 +0800

    added MVVM in Register Content Page.

--- DirectoryApp/ViewModel/Student.cs
+++ DirectoryApp/ViewModel/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace DirectoryApp.ViewModel
 {
@@ -15,9 +16,38 @@ namespace DirectoryApp.ViewModel
         public DateTime BirthDate { get; set; }
         public int MobileNumber { get; set; }
         public string City { get; set; }
-
         public List<string> SchoolProgram { get; set; }
 
+        private string _selectedSchoolProgram;
+        public string SelectedSchoolProgram
+        {
+            get { return _selectedSchoolProgram; }
+            set
+            {
+                if (_selectedSchoolProgram != value)
+                {
+                    _selectedSchoolProgram = value;
+                    OnPropertyChanged(nameof(SelectedSchoolProgram));
+
+                    // Update the available courses based on the selected school program
+                    UpdateCourses();
+                }
+            }
+        }
+
+        private string _selectedCourse;
+        public string SelectedCourse
+        {
+            get { return _selectedCourse; }
+            set
+            {
+                if (_selectedCourse != value)
+                {
+                    _selectedCourse = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public List<string> Courses { get; set; }
         public List<string> YearLevel { get; set; }
 
@@ -41,7 +71,7 @@ namespace DirectoryApp.ViewModel
             //list of year level
             YearLevel = new List<string>
             {
-               "",
+               " ",
                "1",
                "2",
                "3",
@@ -50,5 +80,40 @@ namespace DirectoryApp.ViewModel
             };
         }
 
+        private void UpdateCourses()
+        {
+            if (SelectedSchoolProgram == "School of Engineering")
+            {
+                Courses = new List<string>
+                {
+                    "-SELECT-",
+                    "Computer Engineering",
+                    "Electrical Engineering",
+                    "Electronics Engineering"
+                };
+            }
+            else if (SelectedSchoolProgram == "School of Computer Studies")
+            {
+                Courses = new List<string>
+                {
+                    "-SELECT-",
+                    "Computer Science",
+                    "Information Technology"
+                };
+            }
+            else
+            {
+                Courses = new List<string>();
+
+
+                // Notify property changed for AvailableCourses
+                OnPropertyChanged(nameof(Courses));
+            }
+
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
