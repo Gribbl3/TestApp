864ed4f1fba71a4a04a0fdc691fd180e748e1527
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-30 22:16:03 +0800

    Solved error: Entry binding to an int property.

--- TestApp/Model/BaseModel.cs
+++ TestApp/Model/BaseModel.cs
@@ -25,5 +25,41 @@
         public string Course { get; set; }
         public int MobileNumber { get; set; }
 
+        //entry accepts a string, if we bind it to an int property it will cause binding error
+        //so we need to convert it to int
+
+        //the property that we will be binding to the entry
+        public string IdEntry
+        {
+            set
+            {
+                if (int.TryParse(value, out int result))
+                {
+                    Id = result;
+                }
+                else
+                {
+                    Id = 0;
+                }
+            }
+        }
+
+        public string MobileNumberEntry
+        {
+            set
+            {
+                if (int.TryParse(value, out int result))
+                {
+                    MobileNumber = result;
+                }
+                else
+                {
+                    MobileNumber = 0;
+                }
+            }
+        }
+
+
+
     }
 }
--- TestApp/Model/Student.cs
+++ TestApp/Model/Student.cs
@@ -5,12 +5,12 @@
 
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
-        public string Gender => IsMaleCheck ? "Male" : "Female";
         public string YearLevel { get; set; }
         public DateTime BirthDate { get; set; }
         public string City { get; set; }
         public bool IsMaleCheck { get; set; }
         public bool IsFemaleCheck { get; set; }
+        public string Gender => IsMaleCheck ? "Male" : "Female";
         public List<string> YearLevelItemSource { get; set; } = new()
         {
             "First Year",
