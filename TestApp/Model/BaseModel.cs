c2bcfe56f126bd4b91300e18efa190eb00fc2dac
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-30 22:36:36 +0800

    Added an in-depth explaination of how to bind an int property.

--- TestApp/Model/BaseModel.cs
+++ TestApp/Model/BaseModel.cs
@@ -17,13 +17,13 @@
 
         public List<string> CourseItemSource { get; set; } = new();
 
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string SchoolProgram { get; set; }
         public string Course { get; set; }
-        public int MobileNumber { get; set; }
+        public int? MobileNumber { get; set; }
 
         //entry accepts a string, if we bind it to an int property it will cause binding error
         //so we need to convert it to int
@@ -31,35 +31,50 @@
         //the property that we will be binding to the entry
         public string IdEntry
         {
+            get
+            {
+
+                if (Id == 0)
+                {
+                    return string.Empty;
+                }
+                //when id is null, its to string value is still string.Empty
+                return Id.ToString();
+            }
             set
             {
-                if (int.TryParse(value, out int result))
+                try
                 {
-                    Id = result;
+                    Id = int.Parse(value);
                 }
-                else
+                catch (Exception)
                 {
-                    Id = 0;
+                    Id = null;
                 }
             }
         }
 
         public string MobileNumberEntry
         {
+            get
+            {
+                if (MobileNumber == 0)
+                {
+                    return string.Empty;
+                }
+                return MobileNumber.ToString();
+            }
             set
             {
-                if (int.TryParse(value, out int result))
+                try
                 {
-                    MobileNumber = result;
+                    MobileNumber = int.Parse(value);
                 }
-                else
+                catch (Exception)
                 {
-                    MobileNumber = 0;
+                    MobileNumber = null;
                 }
             }
         }
-
-
-
     }
 }
