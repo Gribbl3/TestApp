0e1726a56f223f569336d87de853fc393c4c5430
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-30 22:16:40 +0800

    Add service for serializing and desearilizing JSON file.

--- TestApp/Service/StudentService.cs
+++ TestApp/Service/StudentService.cs
@@ -1,12 +1,28 @@
-﻿using TestApp.Model;
+﻿using System.Text.Json;
+using TestApp.Model;
 
 namespace TestApp.Service
 {
     public class StudentService : IStudentService
     {
+        readonly string mainDir = FileSystem.Current.AppDataDirectory;
+
         public Task<bool> AddStudent(Student student)
         {
-            throw new NotImplementedException();
+            if (student == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            string fileName = Path.Combine(mainDir, "student.json");
+            if (File.Exists(fileName))
+            {
+                var json = JsonSerializer.Serialize(student);
+                File.WriteAllText(fileName, json);
+
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
         }
 
         public Task<List<Student>> GetStudentList()
