cccd5efdbe21c953617679e9d8dc0f6a37131907
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-10-01 19:23:25 +0800

    Added routes for pages.

--- TestApp/AppShell.xaml.cs
+++ TestApp/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace TestApp
+﻿using TestApp.View;
+
+namespace TestApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(AddContact), typeof(AddContact));
+            Routing.RegisterRoute(nameof(Home), typeof(Home));
+            Routing.RegisterRoute(nameof(Register), typeof(Register));
         }
     }
 }
