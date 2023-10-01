9ec2492468c563d9c0e523b7202105ce2b801fe1
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-10-01 13:53:00 +0800

    Added Type property.

--- TestApp/Model/Contact.cs
+++ TestApp/Model/Contact.cs
@@ -2,6 +2,15 @@
 {
     public class Contact : BaseModel
     {
-
+        private string _type;
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
